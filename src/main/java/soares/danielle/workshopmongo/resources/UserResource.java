60c344cf3970dc377b316543427dcb7065fa005a
Danielle Soares <silvasoaresdanielle2@gmail.com>
2024-10-03 16:08:37 -0300

    user delete

--- src/main/java/soares/danielle/workshopmongo/resources/UserResource.java
+++ src/main/java/soares/danielle/workshopmongo/resources/UserResource.java
@@ -6,12 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import soares.danielle.workshopmongo.domain.User;
@@ -46,6 +41,12 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 		// created retorna o status 201 (quando um obj é criado)
 	}
-
-
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+		// no content é pra quando não há conteúdo para retornar. Código 204;
+	}
+	
 }
